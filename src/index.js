eb5fcbbba2d8992fec53ae82c31c129cdbea69e3
eduardoitoeste <eduardoitoeste@gmail.com>
2020-11-24 09:27:52 -0300

    libreria local

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Ws from '@adonisjs/websocket-client';
-
+// import Ws from '@adonisjs/websocket-client';
+import Ws from '../lib/adonisjs/websocket-client';
 export default class SocketConnection {
 	constructor(config){
 		// this.wsProtocol = config.wsProtocol
