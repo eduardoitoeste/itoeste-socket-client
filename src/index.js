10d37dd5eccb0e86eb80669dfe35596621512643
eduardoitoeste <eduardoitoeste@gmail.com>
2020-11-30 08:21:29 -0300

    versiion 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Ws from '@adonisjs/websocket-client';
-// import Ws from '../lib//adonisjs/websocket-client';
+import io from 'socket.io-client';
+// const io = require('socket.io-client');
 class SocketConnection {
 	constructor(config,callback=""){
 		// this.wsProtocol = config.wsProtocol
@@ -8,83 +8,104 @@ class SocketConnection {
 		this.debug = config.debug ? true : false
 		this.channelPrefix = ""
 		this.subscriptions = []
+		this.callback = callback
 		this.connect()
+		this.interval = null
 	}
 
-	connect () {
+	connect(){
 		let that = this
-		this.ws = Ws('ws://157.245.219.22')
-	 	.withApiToken(this.token)
-      	.connect();
+		this.ws = io.connect('http://157.245.219.22', { 
+			'forceNew': true ,
+			query: {
+    			token: that.token
+  			}
+  		});
 
-      	this.ws.on('open', (data) => {
-      		that.debugLog("log","Connection initialized")
-			  that.config()
-			  
-			  let dataRes = {
-					status : 1,
-					data: data
-				}
-				if(that.callback){
-					that.callback(dataRes)
-				}	
-    	});
-
-    	this.ws.on('close', (data) => {
-			  that.debugLog("error","Connection closed")
-			  
-			  let dataRes = {
-				  status : 0,
-				  data: data
-			  }
-			  if(that.callback){
-				that.callback(dataRes)
-			  }
-    	});
-
-
-      	return this.ws
-	}
-
-	config () {
-		let that = this
-		const channel = this.ws.subscribe("config-init");
-		channel.emit('message','init')
-		channel.on('message', message => {
-			that.channelPrefix = message
+  		this.ws.on('connect', (socket) => {
+  			if(this.interval){
+  				clearInterval(this.interval);
+  			}
+		  // let token = socket.handshake.query.token;
+		  that.debugLog("log","Conectado con el servidor")
+		  
+		  that.callback({
+		  	connect:true,
+		  	status:200,
+		  	socket:that.ws.id,
+		  	message:"Conectado"
+		  })
+		  that.config()
 		});
-		return channel
+
+		
+
+		this.ws.on('disconnect', (reason) => {
+		  that.debugLog("error",reason)
+		  if (reason === 'io server disconnect') {
+		    // the disconnection was initiated by the server, you need to reconnect manually
+		    that.debugLog("log","Intentando conectar al servidor...")
+		    // socket.connect();
+		  }
+		  that.channelPrefix = ""
+		  that.reconnect()
+		  that.callback({
+		  	connect:false,
+		  	status:500,
+		  	socket:that.ws.id,
+		  	message:"Desconectado"
+		  })
+		});
+	}
+
+	config(){
+		let that = this
+		that.ws.on('config-init', (data) => {
+		  if(data.status == 401){
+		  	that.callback({
+		  	  connect:false,
+			  status:401,
+			  socket:that.ws.id,
+			  message:data.message
+			})
+
+			that.debugLog("error","Error de autentificacion, sera desconectado del servidor  ERROR : "+data.message)
+			that.ws.disconnect();
+		  }
+		  if(data.status == 200){
+		  	that.channelPrefix = data.message
+		  	that.debugLog("log","Cliente configurado en "+that.channelPrefix)
+
+		  }
+		});
+	}
+
+	reconnect(){
+		let that = this
+		this.interval = setInterval(() => {
+			that.debugLog("log","Intentando reconectar al servidor...")
+  			that.ws.disconnect()
+  			that.connect()
+		}, 1000);
 	}
 
 	subscribeChannel (channelName,callback) {
-		
 		let that = this
 		if(this.channelPrefix == ""){
 			setTimeout(() => {
-				const channel = that.subscribeChannel(channelName,callback);
-			}, 1000)
+				that.debugLog("error","El servidor no ha terminado de configurar")
+				that.config()
+				that.subscribeChannel(channelName,callback)
+			},1000)
 			return
 		}
 
-		const found = this.subscriptions.find(element => element.channel == channelName);
-		
-		let index = this.subscriptions.indexOf( found )
-		if(found){
-			  this.debugLog("log","Ya existe una subscripcion para "+channelName)
-			  let channelTemp = 'channel:'+this.channelPrefix+'-'+channelName
-			  const channel = this.ws.getSubscription(channelTemp);
-			//   channel.on('message', (message) => {
-			// 	callback(message)
-			//  })
-		}else{
-			this.debugLog("log","Creando subscripcion para "+channelName)
-			let channelTemp = 'channel:'+this.channelPrefix+'-'+channelName
-			const channel = this.ws.subscribe(channelTemp);
-			this.subscriptions.push({'channel':channelName,data:channel})
-			channel.on('message', message => {
-				callback(message)
-			});
-		}	
+		let channel = 'channel-'+this.channelPrefix+'-'+channelName
+		that.debugLog("log","Escuchando el canal "+channel)
+		this.ws.on(channel, (data) => {
+		  callback(data)
+		});
+
 	}
 
 	debugLog(type,data){
@@ -107,3 +128,8 @@ class SocketConnection {
 export default function (config,callback="") {
   return new SocketConnection(config,callback)
 }
+
+
+// module.exports = function (config,callback="") {
+//   return new SocketConnection(config,callback)
+// }
